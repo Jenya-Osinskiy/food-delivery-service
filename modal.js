939d3975acd92abe727cc304054e99baff92208c
Jenya Osinskiy <157902732+Jenya-Osinskiy@users.noreply.github.com>
2024-04-04 01:13:56 +0300

    update form

--- modal.js
+++ modal.js
@@ -10,3 +10,23 @@ mobclosebtn .addEventListener('click', (e) => {
     e.preventDefault();
     menumob.classList.remove('is-open');
 })
+
+let specialbtnnow = document.querySelector('.special-btn-now');
+let enjoybtn = document.querySelector('.enjoy-btn');
+let modaloverlay = document.querySelector('.modal-overlay');
+let modalclosebut = document.querySelector('.modal-close-but');
+
+specialbtnnow.addEventListener('click', () => {
+    modaloverlay.classList.add('is-open');
+})
+
+enjoybtn.addEventListener('click', () => {
+    modaloverlay.classList.add('is-open');
+})
+
+modalclosebut.addEventListener('click', (e) => {
+    e.preventDefault();
+    modaloverlay.classList.remove('is-open');
+})
+
+
